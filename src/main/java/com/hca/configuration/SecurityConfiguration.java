6f029cad039db47372af460dac1e875f471b153a
nxhdev2002 <xuanhoangv7c@gmail.com>
2023-12-10 16:57:11 +0700

    update

--- src/main/java/com/hca/configuration/SecurityConfiguration.java
+++ src/main/java/com/hca/configuration/SecurityConfiguration.java
@@ -44,6 +44,7 @@ public class SecurityConfiguration {
 				.authorizeRequests()
 				.antMatchers(
 						"/api/auth/**",
+						"**",
 						"/v3/api-docs/**",
 						"/swagger-ui/**",
 						"/swagger-ui.html",
