667775b011b3d9b6cda7d198604a87aa2fbfb1c1
nxhdev2002 <xuanhoangv7c@gmail.com>
2023-12-17 21:14:24 +0700

    merge

--- src/main/java/com/hca/service/truyen/TruyenService.java
+++ src/main/java/com/hca/service/truyen/TruyenService.java
@@ -1,11 +1,12 @@
 package com.hca.service.truyen;
 
 import com.hca.model.Comic;
+import com.hca.model.ComicContent;
 
 import java.util.List;
 
 public interface TruyenService {
     Comic getTruyen(Long IdTruyen);
-    String getNoiDung(Long IdTruyen, Long IdChapter);
+    ComicContent getNoiDung(Long IdTruyen, Long IdChapter);
     List<Comic> getAllTruyen();
 }
